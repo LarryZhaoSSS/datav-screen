042c82ef8a2aa6485e37c8c18c4825d37c4f2851
LarryKameZhao <larryzhao1994@outlook.com>
2020-08-08 16:52:55 +0800

    row height and header height

--- src/components/base-scroll-list/index.vue
+++ src/components/base-scroll-list/index.vue
@@ -2,17 +2,32 @@
   <div class="base-scroll-list" :id="id">
     <div
       class="base-scroll-list-header"
-      :style="{ backgroundColor: `${config.headerBg}`, height: `${config.headerHeight}px` }"
+      :style="{ backgroundColor: `${config.headerBg}`, height: `${config.headerHeight}px`,fontSize:`${actualConfig.headerFontSize}px`,color:actualConfig.headerColor }"
     >
       <div
         class="header-item base-scroll-list-text"
         v-for="(headerItem, i) in headerData"
         :key="headerItem + i"
-        :style="headerStyleData[i]"
+        :style="{width:`${columnWidths[i]}px`,...headerStyleData[i]}"
         v-html="headerItem"
+        :align="aligns[i]"
       ></div>
     </div>
-    <div class="base-scroll-list-rows"></div>
+    <div
+      class="base-scroll-list-rows"
+      v-for="(rowData,rowIndex) of rowsData"
+      :key="rowIndex"
+      :style="{height:`${rowHeights[rowIndex]}px`,backgroundColor:rowIndex %2===0?rowBg[1]:rowBg[0],fontSize:`${actualConfig.rowFontSize}px`,color:actualConfig.rowColor}"
+    >
+      <div
+        class="base-scroll-list-columns"
+        v-for="(colData,colIndex) of rowData"
+        :key="colIndex"
+        :style="{width:`${columnWidths[colIndex]}px`,...rowStyle[colIndex]}"
+        :align="aligns[colIndex]"
+        v-html="colData"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -35,39 +50,105 @@ export default {
     const config = { ...props.config };
     const headerData = ref([]);
     const headerStyleData = ref([]);
+    const rowStyle = ref([]);
     const actualConfig = ref({});
     const defaultConfig = {
       header: [],
       headerStyle: [],
+      rowStyle: [],
       headerBg: "rgb(90,90,90)",
       headerHeight: 30,
       headerIndex: false,
       headerIndexContent: "#",
-      headerIndexStyle: {},
+      headerIndexStyle: {
+        width: "50px",
+      },
+      rowIndexStyle: {},
+      rowBg: [],
+      rowNum: 10,
+      data: [],
+      aligns: [],
+      rowFontSize: 20,
+      headerFontSize: 24,
+      rowColor: "#333",
+      headerColor: "#666",
     };
+    const columnWidths = ref([]);
+    const rowsData = ref([]);
+    const rowBg = ref([]);
+    const rowHeights = ref([]);
+    const rowNum = ref(defaultConfig.rowNum);
+    const aligns = ref([]);
     const handleHeader = (config) => {
       const _headerData = cloneDeep(config.header);
       const _headerStyle = cloneDeep(config.headerStyle);
+      const _rowsData = cloneDeep(config.data);
+      const _rowStyle = cloneDeep(config.rowStyle);
+      const _aligns = cloneDeep(config.aligns);
       if (config.header.length === 0) {
         return;
       }
       if (config.headerIndex) {
         _headerData.unshift(config.headerIndexContent);
         _headerStyle.unshift(config.headerIndexStyle);
+        _rowStyle.unshift(config.rowIndexStyle);
+        _rowsData.forEach((rows, index) => {
+          rows.unshift(index + 1);
+        });
+        _aligns.unshift("center");
       }
+      const usedWidth = 0;
+      const usedColumNum = 0;
+      const avgWidth =
+        (width.value - usedWidth) / (_headerData.length - usedColumNum);
+      const _columnWidths = new Array(_headerData.length).fill(avgWidth);
+      _headerStyle.forEach((style, index) => {
+        if (style.width) {
+          const headerWidth = +style.width.replace("px", "");
+          _columnWidths[index] = headerWidth;
+        }
+      });
+
+      columnWidths.value = _columnWidths;
+      console.log(_columnWidths);
       headerData.value = _headerData;
       headerStyleData.value = _headerStyle;
+      rowsData.value = _rowsData;
+      rowStyle.value = _rowStyle;
+      aligns.value = _aligns;
+    };
+    const handleRows = (conifg) => {
+      const { headerHeight } = config;
+      const unusedHeight = height.value - headerHeight;
+      rowNum.value = config.rowNum;
+      if (rowNum.value > rowsData.value.length) {
+        rowNum.value = rowsData.value.length;
+      }
+      const avgHeight = unusedHeight / rowNum.value;
+      rowHeights.value = new Array(rowNum.value).fill(avgHeight);
+      console.log(unusedHeight, height, avgHeight);
+      if (config.rowBg.length > 0) {
+        rowBg.value = config.rowBg;
+      }
     };
     onMounted(() => {
-      console.log(width, height);
       const _actualConfig = assign(defaultConfig, config);
+      rowsData.value = _actualConfig.data || [];
       handleHeader(_actualConfig);
+      handleRows(_actualConfig);
       actualConfig.value = _actualConfig;
     });
     return {
       id,
       headerData,
       headerStyleData,
+      columnWidths,
+      rowsData,
+      rowHeights,
+      rowStyle,
+      rowBg,
+      aligns,
+      actualConfig,
     };
   },
 };
@@ -75,6 +156,7 @@ export default {
 
 <style lang="scss" scoped>
 .base-scroll-list {
+  height: 100%;
   .base-scroll-list-text {
     padding: 0 10px;
     white-space: nowrap;
@@ -89,6 +171,12 @@ export default {
   }
   .header-item {
     width: 150px;
+  }
+  .base-scroll-list-rows {
+    display: flex;
+    align-items: center;
+    .base-scroll-list-columns {
+    }
   }
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -13,17 +13,48 @@ export default {
   setup() {
     const config = ref({});
     const header = ["姓名", "年龄", "月薪"];
-    const headerStyle = [{ color: "yellow" }];
+    const headerStyle = [{ color: "yellow", width: "100px" }];
+    const rowStyle = [{ color: "blue" }];
+    const data = [];
+    const rowBg = ["rgb(240,240,240)", "rgb(255,255,255)"];
+    const aligns = ["center", "center"];
+    const headerFontSize = 24;
+    const rowFontSize = 20;
+    const headerColor = "#fff";
+    const rowColor = "#333";
+    for (let i = 0; i < 10; i++) {
+      data.push([
+        "同学" + (i + 1),
+        Math.floor(Math.random() * 10 + 20),
+        Math.floor(Math.random() * 10000 + 10000),
+      ]);
+    }
+    console.log(data);
     config.value = {
       header,
       headerStyle,
+      rowStyle,
       headerBg: "rgb(80,80,80)",
       headerIndex: true,
       headerHeight: "40",
+      data,
+      rowNum: 10,
+      rowBg,
+      aligns,
+      headerFontSize,
+      rowFontSize,
+      headerColor,
+      rowColor,
     };
+
     return {
       config,
     };
   },
 };
 </script>
+<style lang="scss" scoped>
+.about {
+  height: 400px;
+}
+</style>
